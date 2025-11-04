b87386d572b9aaa5646957c29d5c93d5659e36db
BraydenNickel <brayden.nickel@edu.sait.ca>
2025-11-04 15:03:03 -0700

    Added student edit functions

--- BlazorApp1/Models/Student.cs
+++ BlazorApp1/Models/Student.cs
@@ -1,13 +1,30 @@
+using System.ComponentModel.DataAnnotations;
 public class Student
 {
     public int Id { get; set; }
+
+    [Required]
     public string FirstName { get; set; }
+
+    [Required]
     public string LastName { get; set; }
+
+    [Required]
     public DateTime DateOfBirth { get; set; }
+
+    [Required]
     public string Gender { get; set; }
+
+    [Range(0.0, 4.0, ErrorMessage = "GPA must be between 0.0 and 4.0")]
     public decimal GPA { get; set; }
+
+    [Range(1, 12, ErrorMessage = "Current Semester must be between 1 and 12")]
     public int CurrentSemester { get; set; }
+
+    [Range(1, 8, ErrorMessage = "Number of Courses must be between 1 and 8")]
     public int NumberOfCourses { get; set; }
+
+    [Required]
     public string ProgramEnrolled { get; set; }
 
     public Student() { }
